5147e1852310dcdba343f0b11d3cb3079edab4ac
Ran2410 <ran241027@gmail.com>
2024-05-20 21:25:26 +0700

    first commit

---
+++ page_admin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="konten">
+            <h3>Hi, <span>Admin</span></h3>
+            <h1>Welcome</h1>
+            <p>ini adalah halaman admin</p>
+            <a href="form_login.php" class="tombol">Login</a>
+            <a href="form_register.php" class="tombol">Register</a>
+            <a href="logout.php" class="tombol">Logout</a>
+        </div>
+    </div>
+</body>
+</html>
